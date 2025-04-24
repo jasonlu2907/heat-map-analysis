1caf1e0dd77b274cad10887fda2299c4dc673052
Ngoc Lu <39860829+jasonlu2907@users.noreply.github.com>
2025-04-24 13:25:25 -0500

    fixed marker not showed in production

--- front-end/src/components/mapComponents/SearchControl.tsx
+++ front-end/src/components/mapComponents/SearchControl.tsx
@@ -5,6 +5,18 @@ import 'leaflet-geosearch/dist/geosearch.css';
 import L from 'leaflet';
 import './SearchControl.css';
 
+// Fix for marker icons in production
+const customIcon = new L.Icon({
+  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+  iconRetinaUrl:
+    'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
 const SearchControl = () => {
   const map = useMap();
   const markerRef = useRef<L.Marker | null>(null);
@@ -28,11 +40,11 @@ const SearchControl = () => {
       showMarker: true,
       keepResult: true,
       marker: {
-        icon: new L.Icon.Default(),
+        icon: customIcon,
         draggable: false,
       },
       searchLabel: 'Enter an address...',
-      notFoundMessage: 'Sorry, we couldn’t find that location.',
+      notFoundMessage: "Sorry, we couldn't find that location.",
       animateZoom: true,
       retainZoomLevel: false,
     });
